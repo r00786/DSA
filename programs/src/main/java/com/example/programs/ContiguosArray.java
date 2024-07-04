df3a13d4d2f4a8f4a1578d4f0e5362a37af20e5c
rohitsharma <rohitsharm344@gmail.com>
2024-07-04 18:10:20 +0530

    programs added

---
+++ programs/src/main/java/com/example/programs/ContiguosArray.java
@@ -0,0 +1,47 @@
+package com.example.programs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [0,1]
+ * Output: 2
+ * Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+ * Example 2:
+ *
+ * Input: nums = [0,1,0]
+ * Output: 2
+ * Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 105
+ * nums[i] is either 0 or 1.
+ */
+public class ContiguosArray {
+    public static void main(String[] arr){
+        System.out.println(new ContiguosArray().findMaxLength(new int[]{0,1,0}));
+    }
+
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap();
+        map.put(0, -1);
+        int maxLen=0, sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i] == 1 ? 1 : -1;
+            if (map.containsKey(sum)) {
+                maxLen = Math.max(maxLen, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+        return maxLen;
+    }
+}

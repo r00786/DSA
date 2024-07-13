308c3656eb26212c31a94ce2b16ca58bd3c721e8
rohitsharma <rohitsharm344@gmail.com>
2024-07-13 17:11:46 +0530

    programs added

---
+++ programs/src/main/java/com/example/programs/PathSum.java
@@ -0,0 +1,56 @@
+package com.example.programs;
+
+import com.example.programs.tree.TreeNode;
+
+/**'url - https://leetcode.com/problems/path-sum/description/
+ * Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+ *
+ * A leaf is a node with no children.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+ * Output: true
+ * Explanation: The root-to-leaf path with the target sum is shown.
+ * Example 2:
+ *
+ *
+ * Input: root = [1,2,3], targetSum = 5
+ * Output: false
+ * Explanation: There two root-to-leaf paths in the tree:
+ * (1 --> 2): The sum is 3.
+ * (1 --> 3): The sum is 4.
+ * There is no root-to-leaf path with sum = 5.
+ * Example 3:
+ *
+ * Input: root = [], targetSum = 0
+ * Output: false
+ * Explanation: Since the tree is empty, there are no root-to-leaf paths.
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [0, 5000].
+ * -1000 <= Node.val <= 1000
+ * -1000 <= targetSum <= 1000
+ */
+
+public class PathSum {
+    public static void main(String[] args) {
+        TreeNode treeNode= new TreeNode(1,new TreeNode(2),null);
+        System.out.println( new PathSum().hasPathSum(treeNode,1));
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null)return false;
+        targetSum-=root.val;
+        if(root.left==null&&root.right==null&&0==targetSum)return true;
+
+        boolean left=hasPathSum(root.left,targetSum);
+        boolean right=hasPathSum(root.right,targetSum);
+        return left||right;
+    }
+}
---
+++ programs/src/main/java/com/example/programs/tree/MaxDepth.java
@@ -0,0 +1,15 @@
+package com.example.programs.tree;
+
+public class MaxDepth {
+
+    public int maxDepth(TreeNode root) {
+        return dfs(root,0);
+
+    }
+    int dfs(TreeNode node,int level){
+        if(node==null)return level;
+        int levelLeft= dfs(node.left,level+1);
+        int levelRight=dfs(node.right,level+1);
+        return Math.max(levelLeft,levelRight);
+    }
+}
